b0086bb0da032fd8e16588483ddb116a85137ace
javatrainees8 <133650089+javatrainees8@users.noreply.github.com>
2023-05-15 22:32:28 +0530

    Add files via upload

---
+++ tt_credentials_input.sql
@@ -0,0 +1,26 @@
+INSERT INTO credentials (user_id, password, role, updated_date, updated_by, created_date, created_by)
+VALUES ("dillipkumar@gmail.com", "$2a$10$Sb876rpRoZzPIssMpQ7BW.0au222qrkoMwxoye/HZdcDSKkYjKE4.", "ROLE_ADMIN", "2023-04-04", "Ravi", "2023-04-04", "Ravi");
+
+INSERT INTO credentials (user_id, password, role, updated_date, updated_by, created_date, created_by)
+VALUES ("ravidiggi@gmail.com", "$2a$10$1CRB7COSUUHNwDA.ryUbg.DRB1o8lGXKVjaVN4Tu.C8ouMTK7Zv62", "ROLE_ADMIN", "2023-04-04", "Ravi", "2023-04-04", "Ravi");
+
+INSERT INTO credentials (user_id, password, role, updated_date, updated_by, created_date, created_by)
+VALUES ("garimasingh@gmail.com", "$2a$10$YCVu91IORM2GUQlOXmJEwumGJL5WUSeM886D/RgfJZ8Ok5A4renyC", "ROLE_ADMIN", "2023-04-04", "Ravi", "2023-04-04", "Ravi");
+
+INSERT INTO credentials (user_id, password, role, updated_date, updated_by, created_date, created_by)
+VALUES ("abhishekkumar@gmail.com", "$2a$10$ao6c1lYE7B9MmJihAbiyL.Ecp3I5m44Ss/VyhmKn9BHEW0uIbn3lC", "ROLE_ADMIN", "2023-04-04", "Ravi", "2023-04-04", "Ravi");
+
+INSERT INTO credentials (user_id, password, role, updated_date, updated_by, created_date, created_by)
+VALUES ("dakkakeziya@gmail.com", "$2a$10$7ZzQYQs9odgBEWmAJv1/D.FRHIldiXB8Cz0RGgZgd8/ctA6E.u.PO", "ROLE_ADMIN", "2023-04-04", "Ravi", "2023-04-04", "Ravi");
+
+INSERT INTO credentials (user_id, password, role, updated_date, updated_by, created_date, created_by)
+VALUES ("ronitsahoo@gmail.com", "$2a$10$.qWcHc2puBwimtsY.fQDK.V816WgwvTRcaKxQ71NYLUM2HPxGtDHy", "ROLE_ADMIN", "2023-04-04", "Ravi", "2023-04-04", "Ravi");
+
+INSERT INTO credentials (user_id, password, role, updated_date, updated_by, created_date, created_by)
+VALUES ("nanditasahoo@gmail.com", "$2a$10$4.HEIMmCz93p30XV/JytneA.qY.K2Dpvlj21/eAHQ24ZAIT.UvQ7i", "ROLE_ADMIN", "2023-04-04", "Ravi", "2023-04-04", "Ravi");
+
+INSERT INTO credentials (user_id, password, role, updated_date, updated_by, created_date, created_by)
+VALUES ("nalinpratap@gmail.com", "$2a$10$EAoP6UBg2j6TYdSxqg.WQuJXayWLSOJDplryBsr7AsQ56v3W0LPuG", "ROLE_ADMIN", "2023-04-04", "Ravi", "2023-04-04", "Ravi");
+
+INSERT INTO credentials (user_id, password, role, updated_date, updated_by, created_date, created_by)
+VALUES ("akashdeep@gmail.com", "$2a$10$az3QS1hYkK6AGnbZOQx8Fu0XUJ396Nr9hd9mUtIwmd5KqnbHBDePG", "ROLE_ADMIN", "2023-04-04", "Ravi", "2023-04-04", "Ravi");
